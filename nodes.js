6e1e3f568e6fd62c9c3125e38c038c5c91e0253e
Jui Godbole <juigodbole@Anupams-MacBook-Pro.local>
2015-08-12 21:23:55 -0700

    version2

--- nodes.js
+++ nodes.js
@@ -1,32 +1,58 @@
 
 angular.module('TreeApp',[]).controller('treeController',['$scope', function ($scope) {
-    $scope.node = {showInputBox: false, child: ''};
+    //$scope.node = {showInputBox: false, child: ''};
 
-    $scope.trees = [
+    $scope.showInputBoxes = true;
+
+    $scope.nodes = [
         {
             label: 'Root Node',
             children: [],
-            node: {
-                showInputBox: false,
-                child: ''
-            }
+            showInputBox: false
         }
     ];
 
-    $scope.$watch('node.showInputBox', function () {
-        $scope.plus = $scope.node.showInputBox ? '[-]' : '[+]';
-    });
+    $scope.plus = '[+]';
+
+    /*$scope.$watch('node.showInputBox', function () {
+        //console.log(this);
+        $scope.plus = $scope.nodes.showInputBox ? '[-]' : '[+]';
+    });*/
+
+    $scope.status = {};
+    $scope.status.showInputBox = true;
 
 
-    $scope.parent = $scope.trees[0].label;
-
-
+    $scope.c = {};
     $scope.addNewNode = function (parent) {
-        console.log(parent);
-        console.log($scope.node.child);
-        $scope.trees.push({label: $scope.node.child})
+        //console.log(parent.label);
+        //console.log($scope.c.childEle);
 
+        var par = searchParent($scope.nodes,parent.label);
+        par.children.push({
+            label: this.node.c.childEle,
+            children: [],
+            showInputBox: false
+        });
+
+        this.node.c.childEle = '';
+        console.log(this.node);
+        this.node.showInputBox = false;
+        this.plus = this.node.showInputBox ? '[-]' : '[+]';
+
+    };
+    
+    $scope.exportToJson = function () {
+        angular.toJson($scope.nodes);
+    };
+
+    function searchParent(myArr, myKey) {
+        for(var i = 0; i < myArr.length; i++){
+            if(myArr[i].label === myKey) return myArr[i];
+            else return searchParent(myArr[i].children, myKey);
+        }
     }
+
 }])
 .directive('ngEnter', function () {
     return function (scope, element, attrs) {
